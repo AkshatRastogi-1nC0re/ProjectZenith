5ec577af8e27bde4570328b1e6273ae87485ffa9
AKSHAT RASTOGI <71638737+AkshatRastogi-1nC0re@users.noreply.github.com>
2021-02-18 19:55:25 +0530

    Update email_automation.py

--- email_automation.py
+++ email_automation.py
@@ -4,5 +4,5 @@ import smtplib
 def send_mail(list_emails, message):
     server=smtplib.SMTP('smtp.gmail.com',587)
     server.starttls()
-    server.login("project.zenith11@gmail.com","testing@123")
+    server.login("","")
     server.sendmail('project.zenith11@gmail.com',list_emails,message)
